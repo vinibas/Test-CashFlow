94d658b896a2b78eec20d5118c53dbb0eef03caf
Vinícius Bastos da Silva <viny.bas@gmail.com>
2025-05-29 15:29:06 -0300

    Creation of the API and unit test projects; Configuration of the vscode launch and tasks for debugging.

---
+++ src/CashFlow.Api/Program.cs
@@ -0,0 +1,13 @@
+using CashFlow.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration, builder.Environment);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startup.Configure(app);
+
+app.Run();
---
+++ src/CashFlow.Api/Startup.cs
@@ -0,0 +1,36 @@
+using HealthChecks.UI.Client;
+using Scalar.AspNetCore;
+
+namespace CashFlow.Api;
+
+public class Startup(IConfiguration configuration, IWebHostEnvironment environment)
+{
+
+    public IConfiguration Configuration { get; } = configuration;
+    public IWebHostEnvironment Environment { get; } = environment;
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddHealthChecks();
+        services.AddOpenApi();
+        // services.RegisterServices(Configuration, Environment);
+    }
+    
+    public void Configure(WebApplication app)
+    {
+        app.MapHealthChecks("/_health", new()
+        {
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+
+        if (Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.MapScalarApiReference("/");
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapGroup("api/v1").MapPost("tst", () => "Ok!!");
+    }
+}
